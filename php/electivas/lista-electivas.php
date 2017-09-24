099a043edeba924d1de6a4baf057c7a23210fb12
Luis <lucho951003@gmail.com>
2017-09-24 13:49:07 -0500

    Tercer commit

--- php/electivas/lista-electivas.php
+++ php/electivas/lista-electivas.php
@@ -35,6 +35,34 @@
 			echo json_encode($datos);
 
 		break;
+		case 'ver-inscritos':
+
+			$id = $datos->id;
+
+			$seleccion = "`users`.`user_nombre`, `users`.`user_codigo`";
+			$tabla = "users_x_electiva
+						INNER JOIN `users` ON `users`.`user_id` = `users_x_electiva`.`user_id`";
+			$condicion = "`users_x_electiva`.`elec_id` = $id";
+
+			$datos = listar($seleccion, $tabla, $condicion);
+
+			$datos = utf8_converter($datos);
+
+			echo json_encode($datos);
+
+		break;
+		case 'profesores':
+
+			$seleccion = "DISTINCT `elec_profesor` as profe_nombre";
+			$tabla = "`electivas`";
+
+			$datos = lista($seleccion, $tabla);
+
+			$datos = utf8_converter($datos);
+
+			echo json_encode($datos);
+
+		break;
 
 	}
 
